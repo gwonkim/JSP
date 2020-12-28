30dce1dd0efe9279bdbcc15da00f29865ca82966
gwonkim <wo41896804@gmail.com>
2020-12-28 20:20:54 +0900

    [수업] 학생 데이터 목록 조회, 이름 조회 등 DB 조회
    데이터 목록 조회
    이름으로 조회하기
    학생 한 명 조회하기
    학생 조회 기능 조합

---
+++ lecture2/src/main/java/net/skhu/controller/StudentController.java
@@ -0,0 +1,58 @@
+package net.skhu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.skhu.mapper.StudentMapper;
+
+@Controller
+public class StudentController {
+
+    @Autowired StudentMapper studentMapper;
+
+    @RequestMapping("student/list")
+    public String list(Model model) {
+        model.addAttribute("students", studentMapper.findAll());
+        return "student/list";
+    }
+
+    @RequestMapping("student/list1")
+    public String list1(Model model, String srchText) {
+        if (srchText == null) srchText = "";
+        model.addAttribute("students", studentMapper.findByName(srchText + "%"));
+        model.addAttribute("srchText", srchText);
+        return "student/list1";
+    }
+
+    @RequestMapping("student/detail")
+    public String detail(Model model, Integer id) {
+        if (id == null) id = 4;
+        model.addAttribute("student", studentMapper.findById(id));
+        return "student/detail";
+    }
+
+    @RequestMapping("student/test1")
+    public String test1(Model model) {
+        return "student/test1";
+    }
+
+    @RequestMapping("student/list2")
+    public String list2(Model model, String srchText) {
+        if (srchText == null) srchText = "";
+        model.addAttribute("students", studentMapper.findByName(srchText + "%"));
+        model.addAttribute("srchText", srchText);
+        return "student/list2";
+    }
+
+    @RequestMapping("student/list3")
+    public String list3(Model model, String srchText) {
+        if (srchText == null) srchText = "";
+        model.addAttribute("students", studentMapper.findByName(srchText + "%"));
+        model.addAttribute("srchText", srchText);
+        return "student/list3";
+    }
+
+
+}
---
+++ lecture2/src/main/webapp/WEB-INF/views/student/list1.jsp
@@ -0,0 +1,50 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <style>
+    div.container { width: 600px; margin: 50px auto; }
+    input { padding: 5px; font-size: 10pt; }
+    button { margin: 10px ; padding: 0.4em 2em; }    
+    thead th { background-color: #eee; }
+    table{ border-collapse: collapse; width: 100%; }
+    td, th { padding: 4px; border: 1px solid lightgray; }
+    td:nth-child(4) { text-align: center; }
+  </style>
+</head>
+<body>
+<div class="container">
+
+  <h1>학생목록</h1>
+  <form>
+    <label>이름</label>
+    <input type="text" name="srchText" value="${ srchText }" placeholder="검색조건" />
+    <button type="submit">조회</button>
+  </form>
+  
+  <table class="table table-bordered table-condensed">
+    <thead>
+      <tr>
+        <th>학번</th>
+        <th>이름</th>
+        <th>학과</th>
+        <th>학년</th>
+      </tr>
+    </thead>
+    <tbody>
+      <c:forEach var="student" items="${ students }">
+        <tr>
+          <td>${ student.studentNumber }</td>
+          <td>${ student.name }</td>
+          <td>${ student.departmentName }</td>
+          <td>${ student.year }</td>
+        </tr>
+      </c:forEach>
+    </tbody>
+  </table>
+
+</div>
+</body>
+</html>
